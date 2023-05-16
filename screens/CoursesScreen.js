a289e602f1d6e6b89d7fc04fa2c323749b28682c
VikPuhaev <vikpuhaev@Viks-MacBook-Pro.local>
2023-05-16 19:11:35 +0200

    Vik design 2

--- screens/CoursesScreen.js
+++ screens/CoursesScreen.js
@@ -1,101 +1,3 @@
-//
-// import React from 'react';
-// import { Button, View, Text, StyleSheet, ScrollView } from 'react-native';
-//
-// function CoursesScreen({ route, navigation }) {
-//     const { faculty } = route.params;
-//
-//     // Data structure containing faculties, semesters, and courses
-//     const faculties = {
-//         'Informatik': {
-//             '1': ['Informatik + Recht (RGG)', 'Theoretische Informatik (THI)', 'Technische Grundlagen der Informatik (TGI)', 'Mathematische Grundlagen der Informatik (MG1)', 'Programmierung 1 (PR1)'],
-//             '2': ['Modellierung (MOD)', 'Informatik + Gesellschaft (RGG)', 'Algorithmen und Datenstrukturen (ADS)', 'Betriebssysteme (OS)', 'Mathematische Grundlagen der Informatik 2 (MG2)', 'Programmierung 2 (PR2)'],
-//             '3': ['Projekt-manage-ment (HCI)', 'Grundl. d. intellig. Systeme (IDS)', 'Datenbanksysteme (IDS)', 'Einführung in Numerical Computing (NUM)', 'Einführende Statistik (EST)', 'Programmiersprachen und -konzepte (PLC)'],
-//             '4': ['Mensch-Computer-Interaktion (HCI)', 'Vertiefung 1', 'Vertiefung 2', 'Einführung in die mathematische Modellierung (MM)', 'Software Engineering 1 (SE1)'],
-//             '5': ['Netzwerktechnologien (NET)', 'Vertiefung 3', 'Vertiefung 4', 'Vertiefung 5', 'Software Engineering 2 (SE2)'],
-//             '6': ['Informa-tions-sicherheit (IS)', 'Erweiterung', 'Softwarepraktikum mit Bachelorarbeit']
-//         },
-//         'Chemie': {
-//             '1': ['Chemie Course 1', 'Chemie Course 2'],
-//             '2': ['Chemie Course 3', 'Chemie Course 4'],
-//             // ... rest of the semesters
-//         },
-//         'Mathematik': {
-//             '1': ['Math Course 1', 'Math Course 2'],
-//             '2': ['Math Course 3', 'Math Course 4'],
-//             // ... rest of the semesters
-//         },
-//         // ... rest of the faculties
-//     };
-//
-//     // Get the semesters for the selected faculty
-//     const semesters = faculties[faculty];
-//
-//     return (
-//         <ScrollView>
-//             {/* Render each semester and its courses */}
-//             {Object.keys(semesters).map(semester => (
-//                 <View key={semester} style={styles.semesterContainer}>
-//                     <Text style={styles.semesterTitle}>Semester {semester}</Text>
-//                     {semesters[semester].map(course => (
-//                         <View key={course} style={styles.courseCard}>
-//                             <Button
-//                                 title={course}
-//                                 onPress={() => navigation.navigate('Course', { course })}
-//                                 color="black" // Set the color of the course name to black
-//                             />
-//                         </View>
-//                     ))}
-//                 </View>
-//             ))}
-//         </ScrollView>
-//     );
-// }
-//
-// const styles = StyleSheet.create({
-//     semesterContainer: {
-//         flex: 1,
-//         marginTop: 10,
-//         marginHorizontal: 10,
-//         backgroundColor: '#ffffff',
-//         borderRadius: 16,
-//         padding: 10,
-//         marginBottom: 8,
-//         shadowColor: "#000",
-//         shadowOffset: {
-//             width: 0,
-//             height: 1,
-//         },
-//         shadowOpacity: 0.22,
-//         shadowRadius: 2.22,
-//         elevation: 3,
-//     },
-//     semesterTitle: {
-//         fontSize: 25,
-//         fontWeight: 'bold',
-//         paddingBottom: 15,
-//         paddingLeft: 10,
-//     },
-//     courseCard: {
-//         flex: 1,
-//         backgroundColor: '#f9c2ff',
-//         borderRadius: 16,
-//         padding: 3,
-//         marginBottom: 8,
-//         shadowColor: "#000",
-//         shadowOffset: {
-//             width: 0,
-//             height: 1,
-//         },
-//         shadowOpacity: 0.22,
-//         shadowRadius: 2.22,
-//         elevation: 3,
-//     },
-// });
-//
-// export default CoursesScreen;
-
-
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableHighlight } from 'react-native';
 
@@ -170,7 +72,7 @@ const styles = StyleSheet.create({
     },
     semesterTitle: {
         fontSize: 25,
-        fontWeight: 'bold',
+        //fontWeight: 'bold',
         paddingBottom: 15,
         paddingLeft: 10,
     },
@@ -179,8 +81,8 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         padding: 10,
         marginBottom: 8,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // justifyContent: 'center',
+        // alignItems: 'center',
     },
     courseTitle: {
         fontSize: 18,
--- screens/FeedbackScreen.js
+++ screens/FeedbackScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, StyleSheet, TouchableHighlight } from 'react-native';
 import Slider from '@react-native-community/slider';
 import { CourseContext } from './CourseContext';
 
@@ -24,7 +24,6 @@ function FeedbackScreen({ route, navigation }) {
             }));
             setSubmitted(false);
             navigation.goBack();
-
         }
     }, [submitted]);
 
@@ -33,20 +32,74 @@ function FeedbackScreen({ route, navigation }) {
     };
 
     return (
-        <View>
-            <Text>Question 1: {rating1}</Text>
-            <Slider value={rating1} onValueChange={setRating1} minimumValue={1} maximumValue={10} step={1} />
+        <View style={styles.container}>
+            <Text style={styles.title}>Provide Feedback</Text>
 
-            <Text>Question 2: {rating2}</Text>
-            <Slider value={rating2} onValueChange={setRating2} minimumValue={1} maximumValue={10} step={1} />
+            <Text style={styles.label}>Difficulty: {rating1}</Text>
+            <Slider value={rating1} onValueChange={setRating1} minimumValue={1} maximumValue={10} step={1} style={styles.slider} />
 
-            <Text>Question 3: {rating3}</Text>
-            <Slider value={rating3} onValueChange={setRating3} minimumValue={1} maximumValue={10} step={1} />
+            <Text style={styles.label}>Question 2: {rating2}</Text>
+            <Slider value={rating2} onValueChange={setRating2} minimumValue={1} maximumValue={10} step={1} style={styles.slider} />
 
-            <Button title="Submit" onPress={submit} />
-            {submitted && <Text>Thank you for your review</Text>}
+            <Text style={styles.label}>Question 3: {rating3}</Text>
+            <Slider value={rating3} onValueChange={setRating3} minimumValue={1} maximumValue={10} step={1} style={styles.slider} />
+
+            <View style={styles.buttonContainer}>
+                <TouchableHighlight
+                    style={styles.button}
+                    underlayColor="#e0dcdc" // Set the background color when the button is pressed
+                    onPress={submit}
+                >
+                    <Text style={styles.buttonText}>Submit</Text>
+                </TouchableHighlight>
+            </View>
+
+            {submitted && <Text style={styles.submittedText}>Thank you for your review</Text>}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#ffffff',
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 10,
+    },
+    slider: {
+        marginBottom: 20,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignSelf: "center",
+    },
+    button: {
+        backgroundColor: '#f9c2ff',
+        borderRadius: 16,
+        padding: 10,
+        marginBottom: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 100,
+    },
+    buttonText:
+        {
+            fontSize: 18,
+        },
+    submittedText: {
+        fontSize: 18,
+        marginTop: 10,
+        color: 'green',
+    },
+});
 
 export default FeedbackScreen;
