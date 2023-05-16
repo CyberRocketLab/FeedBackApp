9fdf52057c6db5c544a84fdd0d1dadf2c2406ec7
VikPuhaev <vikpuhaev@Viks-MacBook-Pro.local>
2023-05-16 16:12:25 +0200

    Vik nasral

--- screens/FacultiesScreen.js
+++ screens/FacultiesScreen.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, FlatList, Text, StyleSheet } from 'react-native';
 
 function FacultiesScreen({ navigation }) {
-    return (
-        <View>
-            {/* Add more faculties as needed */}
-            <Button title="Informatik" onPress={() => navigation.navigate('Courses', { faculty: 'Informatik' })} />
-            <Button title="Chemie" onPress={() => navigation.navigate('Courses', { faculty: 'Chemie' })} />
-            <Button title="Mathematik" onPress={() => navigation.navigate('Courses', { faculty: 'Mathematik' })} />
+    // Add more faculties as needed
+    const faculties = ['Informatik', 'Chemie', 'Mathematik'];
+
+    const renderItem = ({ item }) => (
+        <View style={styles.item}>
+            <Text style={styles.title} onPress={() => navigation.navigate('Courses', { faculty: item })}>{item}</Text>
         </View>
     );
+
+    return (
+        <FlatList
+            data={faculties}
+            renderItem={renderItem}
+            keyExtractor={item => item}
+        />
+    );
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+});
 
 export default FacultiesScreen;
