5ba14ad4b1178080e008c42bd161d696cc8b7061
VikPuhaev <vikpuhaev@Viks-MacBook-Pro.local>
2023-05-16 17:22:44 +0200

    pink design

--- screens/CoursesScreen.js
+++ screens/CoursesScreen.js
@@ -1,5 +1,103 @@
+//
+// import React from 'react';
+// import { Button, View, Text, StyleSheet, ScrollView } from 'react-native';
+//
+// function CoursesScreen({ route, navigation }) {
+//     const { faculty } = route.params;
+//
+//     // Data structure containing faculties, semesters, and courses
+//     const faculties = {
+//         'Informatik': {
+//             '1': ['Informatik + Recht (RGG)', 'Theoretische Informatik (THI)', 'Technische Grundlagen der Informatik (TGI)', 'Mathematische Grundlagen der Informatik (MG1)', 'Programmierung 1 (PR1)'],
+//             '2': ['Modellierung (MOD)', 'Informatik + Gesellschaft (RGG)', 'Algorithmen und Datenstrukturen (ADS)', 'Betriebssysteme (OS)', 'Mathematische Grundlagen der Informatik 2 (MG2)', 'Programmierung 2 (PR2)'],
+//             '3': ['Projekt-manage-ment (HCI)', 'Grundl. d. intellig. Systeme (IDS)', 'Datenbanksysteme (IDS)', 'Einführung in Numerical Computing (NUM)', 'Einführende Statistik (EST)', 'Programmiersprachen und -konzepte (PLC)'],
+//             '4': ['Mensch-Computer-Interaktion (HCI)', 'Vertiefung 1', 'Vertiefung 2', 'Einführung in die mathematische Modellierung (MM)', 'Software Engineering 1 (SE1)'],
+//             '5': ['Netzwerktechnologien (NET)', 'Vertiefung 3', 'Vertiefung 4', 'Vertiefung 5', 'Software Engineering 2 (SE2)'],
+//             '6': ['Informa-tions-sicherheit (IS)', 'Erweiterung', 'Softwarepraktikum mit Bachelorarbeit']
+//         },
+//         'Chemie': {
+//             '1': ['Chemie Course 1', 'Chemie Course 2'],
+//             '2': ['Chemie Course 3', 'Chemie Course 4'],
+//             // ... rest of the semesters
+//         },
+//         'Mathematik': {
+//             '1': ['Math Course 1', 'Math Course 2'],
+//             '2': ['Math Course 3', 'Math Course 4'],
+//             // ... rest of the semesters
+//         },
+//         // ... rest of the faculties
+//     };
+//
+//     // Get the semesters for the selected faculty
+//     const semesters = faculties[faculty];
+//
+//     return (
+//         <ScrollView>
+//             {/* Render each semester and its courses */}
+//             {Object.keys(semesters).map(semester => (
+//                 <View key={semester} style={styles.semesterContainer}>
+//                     <Text style={styles.semesterTitle}>Semester {semester}</Text>
+//                     {semesters[semester].map(course => (
+//                         <View key={course} style={styles.courseCard}>
+//                             <Button
+//                                 title={course}
+//                                 onPress={() => navigation.navigate('Course', { course })}
+//                                 color="black" // Set the color of the course name to black
+//                             />
+//                         </View>
+//                     ))}
+//                 </View>
+//             ))}
+//         </ScrollView>
+//     );
+// }
+//
+// const styles = StyleSheet.create({
+//     semesterContainer: {
+//         flex: 1,
+//         marginTop: 10,
+//         marginHorizontal: 10,
+//         backgroundColor: '#ffffff',
+//         borderRadius: 16,
+//         padding: 10,
+//         marginBottom: 8,
+//         shadowColor: "#000",
+//         shadowOffset: {
+//             width: 0,
+//             height: 1,
+//         },
+//         shadowOpacity: 0.22,
+//         shadowRadius: 2.22,
+//         elevation: 3,
+//     },
+//     semesterTitle: {
+//         fontSize: 25,
+//         fontWeight: 'bold',
+//         paddingBottom: 15,
+//         paddingLeft: 10,
+//     },
+//     courseCard: {
+//         flex: 1,
+//         backgroundColor: '#f9c2ff',
+//         borderRadius: 16,
+//         padding: 3,
+//         marginBottom: 8,
+//         shadowColor: "#000",
+//         shadowOffset: {
+//             width: 0,
+//             height: 1,
+//         },
+//         shadowOpacity: 0.22,
+//         shadowRadius: 2.22,
+//         elevation: 3,
+//     },
+// });
+//
+// export default CoursesScreen;
+
+
 import React from 'react';
-import { Button, View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableHighlight } from 'react-native';
 
 function CoursesScreen({ route, navigation }) {
     const { faculty } = route.params;
@@ -34,14 +132,17 @@ function CoursesScreen({ route, navigation }) {
         <ScrollView>
             {/* Render each semester and its courses */}
             {Object.keys(semesters).map(semester => (
-                <View key={semester}>
+                <View key={semester} style={styles.semesterContainer}>
                     <Text style={styles.semesterTitle}>Semester {semester}</Text>
                     {semesters[semester].map(course => (
-                        <Button
+                        <TouchableHighlight
                             key={course}
-                            title={course}
+                            style={styles.courseCard}
+                            underlayColor="#e0dcdc" // Set the background color when the card is pressed
                             onPress={() => navigation.navigate('Course', { course })}
-                        />
+                        >
+                            <Text style={styles.courseTitle}>{course}</Text>
+                        </TouchableHighlight>
                     ))}
                 </View>
             ))}
@@ -50,12 +151,40 @@ function CoursesScreen({ route, navigation }) {
 }
 
 const styles = StyleSheet.create({
+    semesterContainer: {
+        flex: 1,
+        marginTop: 10,
+        marginHorizontal: 10,
+        backgroundColor:'#ffffff',
+        borderRadius: 16,
+        padding: 10,
+        marginBottom: 8,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 3,
+    },
     semesterTitle: {
-        fontSize: 20,
+        fontSize: 25,
         fontWeight: 'bold',
-        marginTop: 20,
+        paddingBottom: 15,
+        paddingLeft: 10,
+    },
+    courseCard: {
+        backgroundColor: '#f9c2ff',
+        borderRadius: 16,
+        padding: 10,
+        marginBottom: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    courseTitle: {
+        fontSize: 18,
     },
 });
 
 export default CoursesScreen;
-
--- screens/FacultiesScreen.js
+++ screens/FacultiesScreen.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Button, View, FlatList, Text, StyleSheet } from 'react-native';
+import { Button, View, FlatList, Text, StyleSheet, TouchableHighlight } from 'react-native';
 
 function FacultiesScreen({ navigation }) {
     // Add more faculties as needed
     const faculties = ['Informatik', 'Chemie', 'Mathematik'];
 
     const renderItem = ({ item }) => (
-        <View style={styles.item}>
-            <Text style={styles.title} onPress={() => navigation.navigate('Courses', { faculty: item })}>{item}</Text>
-        </View>
+        <TouchableHighlight
+            style={styles.item}
+            underlayColor="#e0dcdc" // Set the background color when the card is pressed
+            onPress={() => navigation.navigate('Courses', { faculty: item })}
+        >
+            <Text style={styles.title}>{item}</Text>
+        </TouchableHighlight>
     );
 
     return (
@@ -16,19 +20,38 @@ function FacultiesScreen({ navigation }) {
             data={faculties}
             renderItem={renderItem}
             keyExtractor={item => item}
+            style={styles.list} // Add this line to apply the list style
         />
     );
 }
 
 const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+        width: '100%',
+        marginTop: 8,
+        paddingHorizontal: 16, // Add horizontal padding to match the CoursesScreen style
+    },
     item: {
         backgroundColor: '#f9c2ff',
-        padding: 20,
+        paddingVertical: 15, // Adjust the vertical padding to match the CoursesScreen style
+        paddingHorizontal: 20, // Adjust the horizontal padding to match the CoursesScreen style
         marginVertical: 8,
-        marginHorizontal: 16,
+        borderRadius: 16, // Add this line to apply the card-like appearance
+        justifyContent: 'center', // Center the text vertically
+        alignItems: 'center', // Center the text horizontally
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 3,
     },
     title: {
-        fontSize: 32,
+        fontSize: 25,
+        fontWeight: "bold", // Add this line to apply the bold font weight
     },
 });
 
