0150131795789cc76403641477fcfcb89bda7ff6
VikPuhaev <vikpuhaev@Viks-MacBook-Pro.local>
2023-05-19 14:05:41 +0200

    updated settings

--- screens/PersonalSettingsScreen.js
+++ screens/PersonalSettingsScreen.js
@@ -1,7 +1,5 @@
-// PersonalSettingsScreen.js
-
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Alert, Image, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, TextInput, Alert, Image, TouchableOpacity, ScrollView } from 'react-native';
 
 function PersonalSettingsScreen() {
     const [name, setName] = useState('Max Müller');
@@ -24,22 +22,22 @@ function PersonalSettingsScreen() {
                 style={styles.userImage}
                 source={require('../image/userImage.png')}
             />
+            <ScrollView style={styles.scrollView}>
+                <Text style={styles.label}>Name:</Text>
+                <TextInput style={styles.input} value={name} onChangeText={setName} />
 
-            <Text style={styles.label}>Name:</Text>
-            <TextInput style={styles.input} value={name} onChangeText={setName} />
+                <Text style={styles.label}>Email:</Text>
+                <TextInput style={styles.input} value={email} onChangeText={setEmail} />
 
-            <Text style={styles.label}>Email:</Text>
-            <TextInput style={styles.input} value={email} onChangeText={setEmail} />
+                <Text style={styles.label}>Address:</Text>
+                <TextInput style={styles.input} value={address} onChangeText={setAddress} />
 
-            <Text style={styles.label}>Address:</Text>
-            <TextInput style={styles.input} value={address} onChangeText={setAddress} />
+                <Text style={styles.label}>Phone:</Text>
+                <TextInput style={styles.input} value={phone} onChangeText={setPhone} />
 
-            <Text style={styles.label}>Phone:</Text>
-            <TextInput style={styles.input} value={phone} onChangeText={setPhone} />
-
-            <Text style={styles.label}>Password:</Text>
-            <TextInput style={styles.input} value={password} onChangeText={setPassword} secureTextEntry />
-
+                <Text style={styles.label}>Password:</Text>
+                <TextInput style={styles.input} value={password} onChangeText={setPassword} secureTextEntry />
+            </ScrollView>
             <TouchableOpacity style={styles.button} onPress={handleSave}>
                 <Text style={styles.buttonText}>Save</Text>
             </TouchableOpacity>
@@ -50,17 +48,21 @@ function PersonalSettingsScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
+        justifyContent: 'space-between',
+        alignItems: 'stretch',
         backgroundColor: '#F5F5F5',
         padding: 20,
         paddingTop: 50,
     },
     userImage: {
+        alignSelf: 'center',
         width: 100,
         height: 100,
         borderRadius: 50,
         marginBottom: 20,
+    },
+    scrollView: {
+        flex: 1,
     },
     label: {
         fontSize: 18,
@@ -77,7 +79,7 @@ const styles = StyleSheet.create({
         paddingLeft: 10,
     },
     button: {
-        // Added button style
+        alignSelf: 'center',
         width: '100%',
         height: 40,
         backgroundColor: '#841584',
@@ -87,7 +89,6 @@ const styles = StyleSheet.create({
         marginTop: 20,
     },
     buttonText: {
-        // Added buttonText style
         color: 'white',
         fontSize: 18,
     },
